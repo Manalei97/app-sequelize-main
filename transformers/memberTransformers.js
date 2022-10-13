b9d6502236a298cd4f40e20c187af292400142f6
Manalei97 <manal.ye.tr@gmail.com>
2022-10-13 15:24:18 +0300

    hash

--- transformers/memberTransformers.js
+++ transformers/memberTransformers.js
@@ -1,7 +1,11 @@
+const { photosTransformer } = require("./photoTransformers")
 var memberTransformer = function (member) {
     // console.log(member)
     // do some changes on member
     delete member.dataValues.password
+    if(member.Photos){
+        member.Photos = photosTransformer(member.Photos)
+    }
     return member
 }
 
--- transformers/tripTransformers.js
+++ transformers/tripTransformers.js
@@ -1,9 +1,13 @@
 const { membersTransformer } = require("./memberTransformers")
+const { photosTransformer } = require("./photoTransformers")
 
 
 var tripTransformer = function(trip){
     if(trip.Members){
         trip.Members = membersTransformer(trip.Members)
+    }
+    if(trip.Photos){
+        trip.Photos = photosTransformer(trip.Photos)
     }
     return trip
 }
